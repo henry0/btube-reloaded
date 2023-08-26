47de41271e5509a6c96a1beeac11366a3b2c25d7
henry0th <henry0@knou.ac.kr>
2023-08-27 08:50:50 +0900

    mongoose model

---
+++ src/init.js
@@ -0,0 +1,11 @@
+import "./db";
+import Video from "./models/Video";
+import app from "./server";
+
+const PORT = 4000;
+
+const handleListening = () => {
+    console.log(`server listening on port http://localhost:${PORT}`)
+}
+
+app.listen(PORT,handleListening);
