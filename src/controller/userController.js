d217d33b2e3ca1ffe99c23171d741a54a57ad0b0
henry0th <henry0@knou.ac.kr>
2023-10-08 08:56:44 +0900

    user

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,4 +1,10 @@
-export const join = (req, res) => res.send("Join User");
+export const getJoin = (req, res) => {
+    console.log("getJoin ::: {}", req.body);
+    res.render("join", { pageTitle: "Join"});
+}
+export const postJoin = (req, res) => {
+    res.redirect("/");
+}
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
 export const login = (req, res) => res.send("Login");
